9576c32ed092e18141ddccadb75cc9d8b0839da7
Rohit Pandey <rohit_pandey@persistent.com>
2021-10-13 20:21:22 +0530

    Initial Commit

---
+++ src/main/java/school/erp/StudentManagementErpApplication.java
@@ -0,0 +1,13 @@
+package school.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/erp/StudentManagementErpApplicationTests.java
@@ -0,0 +1,13 @@
+package school.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
